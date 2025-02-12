9a7d6ec592448f47af2ce0bafa449b425920901e
exksalted <aaronbisnes@gmail.com>
2025-02-12 20:29:46 +0200

    Update hthcalc.js

--- site/hthcalc.js
+++ site/hthcalc.js
@@ -291,9 +291,6 @@ function calculatePairwiseStats() {
                     </div>
                 </div>
             </div>
-            <div class="chart-container">
-                <canvas id="matchupChart"></canvas>
-            </div>
         </div>`;
 }
 
